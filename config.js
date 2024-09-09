51d96c9ca75e3ecff15ed91539d732656d105b8c
codigo-qr-calendario <codigoqrparacalendario@gmail.com>
2024-09-09 11:56:27 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbxbfpnyk2Ksw2zGG2P7Zeil5nut6M2ogwzmAfVBVRDH2nM_8ogKbcV9EWhGjS_FO27V/exec";
